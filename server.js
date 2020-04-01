b0b4fc53ffc8d4a9c05d6889a93c0016ec191d9b
JaysonD <jaduff20@gmail.com>
2020-04-01 18:18:58 -0400

    server now displays correct html pages

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const fs = require("fs");
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
