093e93801cab7ac6eb329c8c7435e8b9ed454425
JaysonD <jaduff20@gmail.com>
2020-04-02 19:28:20 -0400

    added post and function for writing to db

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const PORT = process.env.PORT || 3000;
 const path = require("path");
 const fs = require("fs");
 
+function addToDB(newNote) {
+  fs.writeFile(__dirname + "/db/db.json", function(err,data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end(
+        "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
+      );
+    } else {
+      res.writeHead(200, { "Content-Type": "text/json" });
+      res.end(newNote);
+  }
+})
+};
+
 app.use(
   express.urlencoded({
     extended: true
@@ -36,3 +50,10 @@ app.get("/api/notes", (req, res) => {
     }
   });
 });
+
+app.post("/api/notes", function(req, res) {
+  var userNote = req.body;
+  addToDB(userNote)
+  res.json(userNote);
+});
+
