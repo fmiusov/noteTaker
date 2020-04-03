2efb8fa8bc8523cae30efeddc3e94d991bbb9bca
JaysonD <jaduff20@gmail.com>
2020-04-03 18:09:32 -0400

    app.post successfully creates new json and writes it to db.json, but currently creates a new file instead of appending

--- server.js
+++ server.js
@@ -7,38 +7,22 @@ const fs = require("fs");
 // MIDDLEWARE AND EXPRESS CREATION
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 app.use(express.json());
 
 // ROUTES
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // API ENDPOINTS AND USAGE
 app.get("/api/notes", (req, res) => {
-    fs.readFile(__dirname + "/db/db.json", function(err, data) {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end(
-          "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
-        );
-      } else {
-        res.writeHead(200, { "Content-Type": "text/json" });
-        res.end(data);
-      }
-    });
-  });
-
-app.post("/api/notes", function(req, res) {
-  
-  fs.writeFile(__dirname + "/db/db.json", function(err,userNote) {
-    var userNote = req.body;
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/html" });
       res.end(
@@ -46,12 +30,34 @@ app.post("/api/notes", function(req, res) {
       );
     } else {
       res.writeHead(200, { "Content-Type": "text/json" });
-      res.end(userNote);
-  }
-})
+      res.end(data);
+    }
+  });
+});
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end(
+        "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
+      );
+    } else {
+      res.writeHead(200, { "Content-Type": "text/json" });
+      res.end(data);
+    }
+  });
+  var newNote = req.body;
+  fs.writeFile("db.json", JSON.stringify(newNote), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("File has been created");
+  });
 });
 
 // LISTENING TO PORT
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
