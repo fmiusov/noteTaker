17c919df957d86568ee0e8789ceddbcc3e4a0964
JaysonD <jaduff20@gmail.com>
2020-04-03 16:14:39 -0400

    rolling back to different commit, still trying to troubleshoot error

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const fs = require("fs");
+
+function addToDB(newNote) {
+  fs.writeFile(__dirname + "/db/db.json", function(err,data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end(
+        "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
+      );
+    } else {
+      res.writeHead(200, { "Content-Type": "text/json" });
+      res.end(newNote);
+  }
+})
+};
+
 app.use(
   express.urlencoded({
     extended: true
   })
 );
 app.use(express.json());
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -19,15 +36,21 @@ app.listen(PORT, function() {
 });
 
 app.get("/api/notes", (req, res) => {
-  fs.readFile(__dirname + "/db/db.json", function(err, data) {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/html" });
-      res.end(
-        "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
-      );
-    } else {
-      res.writeHead(200, { "Content-Type": "text/json" });
-      res.end(data);
-    }
+    fs.readFile(__dirname + "/db/db.json", function(err, data) {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end(
+          "<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>"
+        );
+      } else {
+        res.writeHead(200, { "Content-Type": "text/json" });
+        res.end(data);
+      }
+    });
   });
+
+app.post("/api/notes", function(req, res) {
+  var userNote = req.body;
+  addToDB(userNote)
+  res.json(userNote);
 });
